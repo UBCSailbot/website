44d2d0979ad9c6c0fa0a0eb3fe37d5a8a2334836
John Ahn <john.ahn@elasticpath.com>
2023-08-23 19:25:45 -0700

    Add local environment variables and api for boat coordinates send dummy data temporarily

--- pages/api/boatCoordinates.ts
+++ pages/api/boatCoordinates.ts
@@ -3,17 +3,21 @@ import { Request, Response } from 'express';
 
 export default async (req: Request, res: Response) => {
     try {
-        const client = await clientPromise;
-        // TODO: Change the DB name
-        const db = client.db("<DB_NAME>");
+        // ** Uncomment once we create a collection for boat coordinates in MongoDB **
+        // const client = await clientPromise;
+        // // TODO: Change the DB name
+        // const db = client.db("<DB_NAME>");
 
-        // TODO: Change the collection name 
-        const boatCoordinates = await db
-            .collection("<COLLECTION_NAME>")
-            .find({})
-            .toArray();
+        // // TODO: Change the collection name
+        // const boatCoordinates = await db
+        //     .collection("<COLLECTION_NAME>")
+        //     .find({})
+        //     .toArray();
 
-        res.json(boatCoordinates);
+        // res.json(boatCoordinates);
+
+        // Remove this dummy data once the collection above is created. 
+        res.json([{lat: 49.37614179786771, lng: -123.27376619978901}]);
     } catch (e) {
         console.error(e);
     }
