7f5b5db6406d7e959969299ad01281eca76c6b91
John Ahn <john.ahn.es@gmail.com>
2023-09-20 23:39:18 -0700

    Setup testing environment using Cucumber.js (#13)

---
+++ tests/steps/common.ts
@@ -0,0 +1,13 @@
+import { Then } from '@cucumber/cucumber';
+import { expect } from 'chai';
+import { api } from '../shared/classes/api';
+
+Then('the service response is {int}', async function (status: number) {
+    expect(api.response.status).to.eq(status, 'Response status is not as expected')
+})
+
+Then('the service error response is {int}', async function (status: number) {
+    expect(api.error.response.status).to.equal(status, 'Error status is not as expected')
+})
+
+export {}
