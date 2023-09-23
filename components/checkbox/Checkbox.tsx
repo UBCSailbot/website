739aed81b8bd819dce14ab22626bd82fca1af88f
Daniel Kim <danielkim1702@gmail.com>
2023-09-23 16:58:42 -0700

    added 3 default checked checkboxes

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDom from 'react-dom';
-
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 function Checkboxes() {
     return (
-        <FormGroup>
-            <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-            <FormControlLabel required control={<Checkbox />} label="Required" />
-            <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
-        </FormGroup>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Checkbox 1" />
+        <FormControlLabel control={<Checkbox />} label="Checkbox 2" />
+        <FormControlLabel control={<Checkbox />} label="Checkbox 3" />
+      </FormGroup>
     );
-};
+  }
 
 export default Checkboxes;
