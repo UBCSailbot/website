d0b18f45d8514e1a449c60a4ffad0b4e22e49234
Daniel Kim <danielkim1702@gmail.com>
2023-09-29 23:46:21 -0700

    added simple logic to checkboxes

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -6,30 +6,56 @@ import FormControl from '@mui/material/FormControl';
 import Paper from '@mui/material/Paper';
 
 function checkboxes() {
+  function checkChecked(status, checkboxNumber){
+    if(!status){
+      console.log("hello, i am layer " + checkboxNumber + "!");
+    }
+  }
+
+  const [checked1, setChecked1] = React.useState(false);
+  const [checked2, setChecked2] = React.useState(false);
+  const [checked3, setChecked3] = React.useState(false);
+
+  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked1(event.target.checked);
+    checkChecked(checked1, 1);
+  }
+  const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked2(event.target.checked);
+    checkChecked(checked2, 2);
+  }
+  const handleChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked3(event.target.checked);
+    checkChecked(checked3, 3);
+  }
+
   return (
     <Paper elevation={0}>
-    <FormControl component="fieldset">
-      <FormGroup aria-label="position" row>
-      <FormControlLabel
-          value="bottom"
-          control={<Checkbox defaultChecked/>}
-          label="Layer 1"
-          labelPlacement="bottom"
-        />
-        <FormControlLabel
-          value="bottom"
-          control={<Checkbox defaultChecked/>}
-          label="Layer 2"
-          labelPlacement="bottom"
-        />
-        <FormControlLabel
-          value="bottom"
-          control={<Checkbox defaultChecked/>}
-          label="Layer 3"
-          labelPlacement="bottom"
-        />
-      </FormGroup>
-    </FormControl>
+      <FormControl component="fieldset">
+        <FormGroup aria-label="position" row>
+          <FormControlLabel
+            control={<Checkbox
+              checked={checked1}
+              onChange={handleChange1} />}
+            label="Layer 1"
+            labelPlacement="bottom"
+          />
+          <FormControlLabel
+            control={<Checkbox
+              checked={checked2}
+              onChange={handleChange2} />}
+            label="Layer 2"
+            labelPlacement="bottom"
+          />
+          <FormControlLabel
+            control={<Checkbox
+              checked={checked3}
+              onChange={handleChange3} />}
+            label="Layer 3"
+            labelPlacement="bottom"
+          />
+        </FormGroup>
+      </FormControl>
     </Paper>
   );
 }
