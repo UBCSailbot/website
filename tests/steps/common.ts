36e1e2c52a00cb5024c084b04202ce8b25a78aa5
JordanChen123 <jordanthechen@gmail.com>
2023-10-01 15:21:26 -0700

    Fixed common.ts and given.ts

--- tests/steps/common.ts
+++ tests/steps/common.ts
@@ -2,19 +2,17 @@ import { Then } from '@cucumber/cucumber';
 import { expect } from 'chai';
 import { api } from '../shared/classes/api';
 
-/* Then('the service response is {int}', async function (status: number) {
+Then('the service response is {int}', async function (status: number) {
     expect(api.response.status).to.eq(status, 'Response status is not as expected')
-    console.log(api.error.response);
 })
 
 Then('the service error response is {int}', async function (status: number) {
     expect(api.error.response.status).to.equal(status, 'Error status is not as expected')
-    console.log(api.error.response);
 })
-*/
+
 
 Then('the service success response is {int}', async function (status: number) {
-    expect(api.response.status).to.equal(status, 'Success status is as expected')
+    expect(api.response.status).to.equal(status, 'Success status is not as expected')
 })
 
 export {}
--- tests/steps/given.ts
+++ tests/steps/given.ts
@@ -14,26 +14,24 @@ Given('I clear the database', async function () {
 
 Given('I insert GPS data into the database', async function () {
     const gpsData = {
-    latitude: 49.2243,
-    longitude: 4.5552,
-    speed: 30,
-    heading: 45,
+        latitude: 49.2243,
+        longitude: 4.5552,
+        speed: 30,
+        heading: 45,
     };
 
     await GPS.create(gpsData);
 });
 
 Then('the response data matches the data in the database', async function () {
-        const db = await ConnectMongoDB();
+    databaseData = await GPS.find({}).then(function(gps) {
+        let transformedGPS = gps.map((data) => data.toJSON())
+        return transformedGPS;
+    });
 
-        databaseData = await GPS.find({}).then(function(gps) {
-            let transformedGPS = gps.map((data) => data.toJSON())
-            return transformedGPS;
-        });
-
-        apiResponseData = api.response.data.data[0];
-        expect(apiResponseData.latitude).to.equal(databaseData[0].latitude, 'Data in the response does not match data in the database');
-        expect(apiResponseData.longitude).to.equal(databaseData[0].longitude, 'Data in the response does not match data in the database');
-        expect(apiResponseData.speed).to.equal(databaseData[0].speed, 'Data in the response does not match data in the database');
-        expect(apiResponseData.heading).to.equal(databaseData[0].heading, 'Data in the response does not match data in the database');
+    apiResponseData = api.response.data.data[0];
+    expect(apiResponseData.latitude).to.equal(databaseData[0].latitude, 'Data in the response does not match data in the database');
+    expect(apiResponseData.longitude).to.equal(databaseData[0].longitude, 'Data in the response does not match data in the database');
+    expect(apiResponseData.speed).to.equal(databaseData[0].speed, 'Data in the response does not match data in the database');
+    expect(apiResponseData.heading).to.equal(databaseData[0].heading, 'Data in the response does not match data in the database');
 });
