e4051f144ad40a4bcebe1d0b8b102667632abfce
Daniel Kim <danielkim1702@gmail.com>
2023-10-07 12:15:45 -0700

    refactored previous code in class component

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -1,63 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import Paper from '@mui/material/Paper';
 
-function Checkboxes() {
-  function checkChecked(status, checkboxNumber){
-    if(!status){
+class Checkboxes extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+
+    //initializing an array to track button states
+    this.state = {
+      //           button1 button2 button3
+      buttonStates:[false, false, false],
+    };
+  }
+
+  checkChecked(status, checkboxNumber) {
+    if (!status) {
       console.log("hello, i am layer " + checkboxNumber + "!");
     }
   }
 
-  const [checked1, setChecked1] = React.useState(false);
-  const [checked2, setChecked2] = React.useState(false);
-  const [checked3, setChecked3] = React.useState(false);
-
-  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked1(event.target.checked);
-    checkChecked(checked1, 1);
-  }
-  const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked2(event.target.checked);
-    checkChecked(checked2, 2);
-  }
-  const handleChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked3(event.target.checked);
-    checkChecked(checked3, 3);
+  handleButtonClick(index) {
+    let newArr = [...this.state.buttonStates]
+    newArr[index] = !newArr[index]
+    this.setState({buttonStates:newArr})
+    this.checkChecked(this.state.buttonStates[index], ++index)
   }
 
-  return (
-    <Paper elevation={0}>
-      <FormControl component="fieldset">
-        <FormGroup aria-label="position" row>
-          <FormControlLabel
-            control={<Checkbox
-              checked={checked1}
-              onChange={handleChange1} />}
-            label="Layer 1"
-            labelPlacement="bottom"
-          />
-          <FormControlLabel
-            control={<Checkbox
-              checked={checked2}
-              onChange={handleChange2} />}
-            label="Layer 2"
-            labelPlacement="bottom"
-          />
-          <FormControlLabel
-            control={<Checkbox
-              checked={checked3}
-              onChange={handleChange3} />}
-            label="Layer 3"
-            labelPlacement="bottom"
-          />
-        </FormGroup>
-      </FormControl>
-    </Paper>
-  );
+  render() {
+    const { buttonStates } = this.state;
+
+    return (
+      <Paper elevation={0}>
+        <FormControl component="fieldset">
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              control={<Checkbox
+                checked={buttonStates[0]}
+                onChange={() => this.handleButtonClick(0)} />}
+              label="Layer 1"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              control={<Checkbox
+                checked={buttonStates[1]}
+                onChange={() => this.handleButtonClick(1)} />}
+              label="Layer 2"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              control={<Checkbox
+                checked={buttonStates[2]}
+                onChange={() => this.handleButtonClick(2)} />}
+              label="Layer 3"
+              labelPlacement="bottom"
+            />
+          </FormGroup>
+        </FormControl>
+      </Paper>
+    );
+  }
 }
 
 export default Checkboxes;
