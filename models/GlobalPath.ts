792a0659ff15643cb91229735f89c5da1228b581
jahn18 <john.ahn.es@gmail.com>
2023-10-10 20:45:33 -0700

    Update the model for Global Path

--- models/GlobalPath.ts
+++ models/GlobalPath.ts
@@ -11,9 +11,22 @@ export interface GlobalPath extends mongoose.Document {
 
 const GlobalPathSchema = new mongoose.Schema<GlobalPath>({
     waypoints: {
-        type: WayPoint[],
+        type: [
+            {
+                latitude: mongoose.Types.Decimal128,
+                longitude: mongoose.Types.Decimal128
+            }
+        ],
         required: [true, "Missing array of objects in GlobalPath interface"]
     }
 });
+
+GlobalPathSchema.set('toJSON', {
+    transform: (doc, ret) => {
+       // @ts-ignore: Expected 3 arguments, but got 1
+      decimal2JSON(ret);
+      return ret;
+    }
+  });
 
 export default mongoose.models.GlobalPath || mongoose.model<GlobalPath>("GlobalPath", GlobalPathSchema);
