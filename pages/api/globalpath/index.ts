4799b090a6c35d0f38f3c2f3c4c163a0fb3292bf
Daniel Kim <danielkim1702@gmail.com>
2023-10-15 16:16:38 -0700

    removing mongoDB id portion when querying DB

--- pages/api/globalpath/index.ts
+++ pages/api/globalpath/index.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import ConnectMongoDB from '@/lib/mongodb';
-import GPS from '@/models/GlobalPath';
 import GlobalPath from '@/models/GlobalPath';
 
 export default async function handler (
@@ -16,7 +15,7 @@ export default async function handler (
   switch (method) {
     case 'GET':
       try {
-        const gPath = await GlobalPath.find({}).select('-_id -__v');
+        const gPath = await GlobalPath.find({}).select({ 'waypoints._id': 0, '_id': 0, '__v': 0 });
         res.status(200).json({ success: true, data: gPath });
       } catch (error) {
         res.status(400).json({ success: false, message: error.message });
