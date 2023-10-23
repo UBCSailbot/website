fcab1b5cfc7ef94c60451afc836d2a71e1a1266b
Jordan Chen <122140935+JordanChen123@users.noreply.github.com>
2023-10-23 11:47:54 -0700

    Adding AISShips Data to Redux Store (#33)
    * Adding AISShips Data to Redux Store
    
    * Fixed types and reducers
    
    * Fixed

--- lib/redux/rootSaga.ts
+++ lib/redux/rootSaga.ts
@@ -1,9 +1,13 @@
 import GPSActions from "@/stores/GPS/GPSActions";
 import gpsSaga from "@/stores/GPS/GPSSagas";
+import AISShipsActions from "@/stores/AISShips/AISShipsActions";
+import aisShipsSaga from "@/stores/AISShips/AISShipsSagas";
+
 import { all, call } from "redux-saga/effects";
 
 export function* rootSaga() {
     yield all([
         gpsSaga[GPSActions.POLL_GPS](),
+        aisShipsSaga[AISShipsActions.POLL_AISSHIPS](),
     ]);
 }
