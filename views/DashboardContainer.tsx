10e385c2bab94a70df562a0cc72fd45642416a57
JordanChen123 <jordanthechen@gmail.com>
2023-11-25 21:54:24 -0800

    Created a LineChart Component

--- views/DashboardContainer.tsx
+++ views/DashboardContainer.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import LineChartComponent from './components/LineChart/LineChart';
 
 export interface DashboardContainerProps {}
 
 class DashboardContainer extends React.PureComponent<DashboardContainerProps> {
   render() {
+
+    const sampleData = [
+      { exampleXKey: 1, exampleYKey: 200 },
+      { exampleXKey: 2, exampleYKey: 300 },
+    ];
+
+    const xAxisKey = 'exampleXKey';
+    const yAxisKey = 'exampleYKey';
+
     return (
       <div>
         <h1>Dashboard Page</h1>
+        <LineChartComponent
+          data={sampleData}
+          xAxisKey={xAxisKey}
+          yAxisKey={yAxisKey}
+        />
       </div>
     );
   }
 }
 
 const mapStateToProps = (state: any) => ({});
+
 const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
