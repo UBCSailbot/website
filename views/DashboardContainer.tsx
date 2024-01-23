db14507dba8272effd588d58b3bfbb91d60e874d
Jordan Chen <122140935+JordanChen123@users.noreply.github.com>
2024-01-22 16:03:44 -0800

    Created a LineChart Component (#76)
    * Created a LineChart Component
    
    * visualizing GPS speed vs timestamp data
    
    * LineCharts for GPS, Batteries, and WindSensor Data
    
    * Created a LineChart Component
    
    * visualizing GPS speed vs timestamp data
    
    * LineCharts for GPS, Batteries, and WindSensor Data
    
    * Rebased WindSensors and fixed other changes
    
    * Revert "Rebased WindSensors and fixed other changes"
    
    This reverts commit 606d28675dcc98bde766252aaee8af4808165684.

--- views/DashboardContainer.tsx
+++ views/DashboardContainer.tsx
@@ -1,19 +1,82 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import LineChartComponent from './components/LineChart/LineChart';
+import MultiLineChartComponent from './components/LineChart/MultiLineChart';
+import { GPS } from '@/stores/GPS/GPSTypes';
+import { Batteries } from '@/stores/Batteries/BatteriesTypes';
+import { WindSensors } from '@/stores/WindSensors/WindSensorsTypes';
 
-export interface DashboardContainerProps {}
+export interface DashboardContainerProps {
+  gpsData: GPS[]
+  batteriesData: Batteries[]
+  windSensorsData: WindSensors[]
+}
 
 class DashboardContainer extends React.PureComponent<DashboardContainerProps> {
   render() {
+
+    const { gps, batteries, windSensors } = this.props;
+
+    const gpsChartData = gps.data.map(gpsPoint => ({
+      timestamp: gpsPoint.timestamp,
+      speed: gpsPoint.speed
+    }));
+
+   const batteriesChartDataVoltage = batteries.data.map((batteriesPoint) => ({
+      timestamp: batteriesPoint.timestamp,
+      battery1Voltage: batteriesPoint.batteries[0].voltage,
+      battery2Voltage: batteriesPoint.batteries[1].voltage
+    }));
+
+    const batteriesChartDataCurrent = batteries.data.map((batteriesPoint) => ({
+      timestamp: batteriesPoint.timestamp,
+      battery1Current: batteriesPoint.batteries[0].current,
+      Battery2Current: batteriesPoint.batteries[1].current
+    }));
+
+    const windSensorsChartData = windSensors.data.map((windSensorsPoint) => ({
+      timestamp: windSensorsPoint.timestamp,
+      windSensor1Speed: windSensorsPoint.windSensors[0].speed,
+      windSensor22Speed: windSensorsPoint.windSensors[1].speed,
+    }));
+
     return (
       <div>
         <h1>Dashboard Page</h1>
+        <LineChartComponent
+          data={gpsChartData}
+          xAxisKey="timestamp"
+          yAxisKey="speed"
+        />
+        <MultiLineChartComponent
+          data={batteriesChartDataVoltage}
+          xAxisKey="timestamp"
+          yAxisKey1="battery1Voltage"
+          yAxisKey2="battery2Voltage"
+        />
+        <MultiLineChartComponent
+          data={batteriesChartDataCurrent}
+          xAxisKey="timestamp"
+          yAxisKey1="battery1Current"
+          yAxisKey2="battery2Current"
+        />
+        <MultiLineChartComponent
+          data={windSensorsChartData}
+          xAxisKey="timestamp"
+          yAxisKey1="windSensor1Speed"
+          yAxisKey2="windSensor2Speed"
+        />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state: any) => ({});
+const mapStateToProps = (state: any) => ({
+  gpsData: state.gps,
+  batteriesData: state.batteries,
+  windSensorsData: state.windSensors
+});
+
 const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
