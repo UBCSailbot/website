01eaddd80619533dd8069c6bc17c0396ff8d3693
fyang151 <fyang151@gmail.com>
2024-01-27 14:09:33 -0800

    hi

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -7,5 +7,6 @@ import { Provider } from 'react-redux';
 import { reduxStore } from '@/lib/redux';
 
 export const Providers = (props: React.PropsWithChildren) => {
+  // this works very well
   return <Provider store={reduxStore}>{props.children}</Provider>;
 };
